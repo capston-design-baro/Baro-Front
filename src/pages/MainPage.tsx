e1c823c0b19548dd1c7430369cdec16841003638
soyeon1806 <ksy1118s@naver.com>
2025-10-01 18:14:38 +0900

    ✨ Feat: 메인 페이지 레이아웃 구성

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,16 @@
+import ServiceSection from '@/sections/ServiceSection/ServiceSection';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function MainPage() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="min-h-screen">
+      {/* 상단 헤더 배치 */}
+      {/* <Header /> */}
+      <ServiceSection onClickCard={(s) => s.to && navigate(s.to)} />
+      {/* <Footer /> */}
+    </div>
+  );
+}
