23cd3654df08101ff820cc08a3eb5ef3d9e2bc1d
soyeon1806 <ksy1118s@naver.com>
2025-10-01 18:19:30 +0900

    🐛 Fix: MainPage에서 불필요한 React import 제거

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,4 @@
 import ServiceSection from '@/sections/ServiceSection/ServiceSection';
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function MainPage() {
