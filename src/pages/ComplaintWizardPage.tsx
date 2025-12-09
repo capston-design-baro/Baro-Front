6c3487dd1478505a523edc573136680b9e2e69bd
soyeon1806 <ksy1118s@naver.com>
2025-12-10 03:36:21 +0900

    💄 Style: 반응형 크기 조절

--- src/pages/ComplaintWizardPage.tsx
+++ src/pages/ComplaintWizardPage.tsx
@@ -379,7 +379,7 @@ const ComplaintWizardPage: React.FC = () => {
                 />
               </div>
 
-              <aside className="rounded-200 bg-neutral-0 mt-2 h-[498px] w-[340px] border border-neutral-200 p-4">
+              <aside className="rounded-200 bg-neutral-0 mt-2 h-[498px] w-[340px] border border-neutral-200 p-4 xl:h-[576px]">
                 <h2 className="text-body-1-bold mb-2">AI가 찾은 핵심 키워드</h2>
                 <p className="text-body-3-regular mb-4 text-neutral-700">
                   {ragKeyword
