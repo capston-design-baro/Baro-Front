7e7a3b6c7fc6dcf2a13067df2a4fbac8497844d2
soyeon1806 <ksy1118s@naver.com>
2025-12-12 02:44:23 +0900

    🐛 Fix: 라벨 이름 수정

--- src/sections/ComplaintExtraInfoSection.tsx
+++ src/sections/ComplaintExtraInfoSection.tsx
@@ -240,7 +240,7 @@ const ComplainantExtraInfoSection = forwardRef<ComplainantExtraInfoSectionHandle
           <div className="flex flex-col gap-2">
             <div className="flex items-center justify-between">
               {/* 주소 라벨 */}
-              {renderLabel('주소', true)}
+              {renderLabel('사무실 주소', true)}
 
               {/* 주소 검색 버튼 */}
               <DaumPostcodeButton onSelect={handleAddressSelect} />
